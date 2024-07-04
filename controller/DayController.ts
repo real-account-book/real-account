9efcf551dc0c1b3d80f1040dff9582ef675b3f6a
paengjiwoo <shishalu04@gmail.com>
2024-07-04 12:58:22 +0900

    :sparkles: feat: 일별 전체 입금 내역 조회 API 구현

--- controller/DayController.ts
+++ controller/DayController.ts
@@ -1,22 +1,32 @@
-import conn from "../db"
-import { Request,Response } from "express"
-import { StatusCodes } from "http-status-codes"
+import conn from "../db";
+import { Request,Response } from "express";
+import { StatusCodes } from "http-status-codes";
 
-export const getMonthPlus = (req : Request,res : Response) => {
+export const getDayPlus = (req : Request,res : Response) => {
+  let date = String(req.query.date);
+  const regex = /^(19|20)\d\d-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
+  if (!date || !regex.test(date)) return res.status(StatusCodes.BAD_REQUEST).end();
+
+  const [year, month, day] = date.split('-');
+  let query = `SELECT *
+                FROM Asset_plus
+                WHERE YEAR(uploaded_at) = ?
+                  AND MONTH(uploaded_at) = ?
+                  AND DAY(uploaded_at) = ?`;
+  let values = [year, month, day];
+  conn.query(query, values, (_err, result) => {
+      return res.status(StatusCodes.OK).json(result)
+  })
+};
+
+export const getDayMinus = (req : Request,res : Response) => {
   let query = ''
   conn.query(query, (_err, result) => {
       return res.status(StatusCodes.OK).json(result)
   })
 };
 
-export const getMonthMinus = (req : Request,res : Response) => {
-  let query = ''
-  conn.query(query, (_err, result) => {
-      return res.status(StatusCodes.OK).json(result)
-  })
-};
-
-export const getMonthTotal = (req : Request,res : Response) => {
+export const getDayTotal = (req : Request,res : Response) => {
   let query = ''
   conn.query(query, (_err, result) => {
       return res.status(StatusCodes.OK).json(result)
