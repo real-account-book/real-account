e06ce41752edadbb0c619442f63711510479cc84
팽지우 <122685653+paengjiwoo@users.noreply.github.com>
2024-07-04 15:35:53 +0900

    :ambulance: HOTFIX: app.ts 수정

--- app.ts
+++ app.ts
@@ -1,14 +1,17 @@
 import express, { Express, Request, Response } from "express";
 import {monthRouter} from './routes/month'
 import { plusRouter } from "./routes/plus";
+import { dayRouter } from "./routes/day";
+import { minusRouter } from "./routes/minus";
 
 const app: Express = express();
 const port = 8888;
 
-
-
 app.use('/month', monthRouter);
 app.use('/plus', plusRouter);
+
+app.use('/day', dayRouter);
+app.use('/minus', minusRouter);
 
 
 app.listen(port, ()=> {
