9e3393420d576c0376025a6d3eb4e272a3ca5b7a
InitialUUU <124362567+InitialUUU@users.noreply.github.com>
2024-07-06 15:59:39 +0900

    ✨init: 타입스크립트 연, 카테고리 부분 기본기능 구현 240706 16:00

--- app.ts
+++ app.ts
@@ -1,16 +1,16 @@
 import express, { Express, Request, Response } from "express";
-import {monthRouter} from './routes/month'
+import {monthRouter} from './routes/month';
 import { plusRouter } from "./routes/plus";
-import mysql from 'mysql2';
+import { yearRouter } from "./routes/year";
+import { categoryRouter } from "./routes/category";
 
 const app: Express = express();
 const port = 8888;
 
-
-
 app.use('/month', monthRouter);
 app.use('/plus', plusRouter);
-
+app.use('/year', yearRouter);
+app.use('/category', categoryRouter);
 
 app.listen(port, ()=> {
     console.log(`[server]: Server is running at <https://localhost:${port}>`)
---
+++ routes/category.ts
@@ -0,0 +1,11 @@
+import express, {Router} from "express";
+import { getCategoryMinus, getCategoryMinusDetail, addCategoryMinus } from "../controller/CategoryController";
+
+export const categoryRouter : Router = express.Router()
+categoryRouter.use(express.json());
+
+categoryRouter.get('/categoryMinus', getCategoryMinus)
+categoryRouter.get('/categoryMinusDetail', getCategoryMinusDetail)
+
+categoryRouter.route('/')
+.post(addCategoryMinus)
