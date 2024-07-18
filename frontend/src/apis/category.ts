76fe3cf1da53c0fa74d3b2e267021b65668d6a13
paengjiwoo <shishalu04@gmail.com>
2024-07-18 19:41:01 +0900

    :zap: feat: category API fetch 개선
    - async await 활용한 비동기성 추가
    - fetch 활용으로 인한 HTTP Error 수동 핸들링 추가
    - try catch 활용해 에러 핸들링

--- frontend/src/apis/category.ts
+++ frontend/src/apis/category.ts
@@ -2,39 +2,72 @@ import axios from 'axios';
 import { BASE_URL } from "./BASE_URL";
 const CATEGORY_URL = `${BASE_URL}/category`;
 
+{/* types*/}
+type TGetCategoryProps = {
+  category_id: number
+}
+
+type TAddCategoryProps = {
+  category_name: string
+}
+
 // 카테고리 전체 조회
 export type TGetAllCategories = () => Promise<any>;
 // 카테고리 개별 조회
-export type TGetCategory = (categodyId: number) => Promise<any>;
+export type TGetCategory = (payload: TGetCategoryProps) => Promise<any>;
 // 카테고리 항목 추가
-export type TAddCategory = (category_name: string) => Promise<any>;
+export type TAddCategory = (payload: TAddCategoryProps) => Promise<any>;
 
 
+{/* logics */}
 // 카테고리 전체 조회
-export const getAllCategories: TGetAllCategories = () => {
-  return fetch(CATEGORY_URL, {
-    method: 'GET',
-    headers: {'Content-Type': 'application/json',},
-  }).then((res) => res.json())
-  .catch((err) => { throw err })
+export const getAllCategories: TGetAllCategories = async () => {
+  try {
+    const response = await fetch(CATEGORY_URL, {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json',},
+    });
+    if (!response.ok) {
+      throw new Error(`Error status: ${response.status}`)
+    }
+    return await response.json();
+  } catch (err) {
+    console.error("Error fetching getAllCategories:", err);
+    throw err;
+  }
 };
 
 // 카테고리 개별 조회
-export const getCategory: TGetCategory = (categodyId) => {
-  return fetch(`${CATEGORY_URL}/${categodyId}`, {
-    method: 'GET',
-    headers: {'Content-Type': 'application/json',},
-  }).then((res) => res.json())
-  .catch((err) => { throw err })
+export const getCategory: TGetCategory = async (payload) => {
+  try {
+    const response = await fetch(`${CATEGORY_URL}/${payload.category_id}`, {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json',},
+    })
+    if (!response.ok) {
+      throw new Error(`Error status: ${response.status}`)
+    }
+    return await response.json();
+  } catch (err) {
+    console.error("Error fetching getCategory:", err);
+    throw err;
+  }
 };
 
 // 카테고리 항목 추가
-export const addMinus: TAddCategory = (category_name) => {
-  const data = { category_name }
-  return fetch(CATEGORY_URL, {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json',},
-    body: JSON.stringify(data),
-  }).then((res) => console.log(res.json()))
-  .catch((err) => console.log(err))
+export const addCategory: TAddCategory =  async (payload) => {
+  try {
+    const response = await fetch(CATEGORY_URL, {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json',},
+      body: JSON.stringify(payload)
+    })
+    if (!response.ok) {
+      throw new Error(`Error status: ${response.status}`)
+    }
+    return await response.json();
+  } catch (err) {
+    console.error("Error fetching addCategory:", err);
+    throw err;
+  }
 }
