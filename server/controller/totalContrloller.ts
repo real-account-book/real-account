83dcd74aedf8c1223f5539f9fbfd3de48f3e47d8
vancouver <kjh03291@gmail.com>
2024-07-22 15:13:59 +0900

    :fire: total id값 추출, category validator 삭제

--- server/controller/totalContrloller.ts
+++ server/controller/totalContrloller.ts
@@ -6,12 +6,11 @@ import { Request, Response } from 'express';
 
 export const getTotalPlus = async (req : Request,res : Response) => {
 
-    // body -> params
     const {start_at, end_at} = req.params; 
     const plusRepository = AppDataSource.getRepository(Asset_plus)
     try{
         const query = plusRepository.createQueryBuilder('asset_plus')
-        .select(['asset_plus.uploaded_at','asset_plus.plus', 'asset_plus.title', 'asset_plus.content'])
+        .select(['asset_plus.plus_id','asset_plus.uploaded_at','asset_plus.plus', 'asset_plus.title', 'asset_plus.content'])
         .where(`asset_plus.uploaded_at >= :start_at AND asset_plus.uploaded_at <= :end_at`, {  start_at: start_at, end_at: end_at })
         const getTotalPlus = await query.getMany();
 
@@ -33,7 +32,7 @@ export const getTotalMinus = async (req : Request,res : Response) => {
     
     try{
         const query =  minusRepository.createQueryBuilder('asset_minus')
-        .select(['asset_minus.uploaded_at','asset_minus.minus', 'asset_minus.title', 'asset_minus.content'])
+        .select(['asset_minus.minus_id','asset_minus.uploaded_at','asset_minus.minus', 'asset_minus.title', 'asset_minus.content'])
         .leftJoinAndSelect("asset_minus.category","category")
         .addSelect('category.category_name')
         .where(`asset_minus.uploaded_at >= :start_at AND asset_minus.uploaded_at <= :end_at`, {  start_at: start_at, end_at: end_at })
